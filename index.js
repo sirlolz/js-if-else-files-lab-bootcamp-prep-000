c98fe4209e05157829d818704652e838fa348934
Jared Burnham <zlolris97@gmail.com>
2019-06-17 00:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ module.exports = { name, height
 if (name==="susan"){
   console.log("The name is correct");
 }else{
-  console.log("Expected the name to be susan")
+  console.log("Expected the name to be susan");
 }
